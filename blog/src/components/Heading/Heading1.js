ba76458c00e98a0fa8d0c913a1e548f67a89bb78
Chue Xiong <xiongce@gmatc.matc.edu>
2025-04-03 20:15:04 -0500

    Completed Activity-7

--- blog/src/components/Heading/Heading1.js
+++ blog/src/components/Heading/Heading1.js
@@ -4,4 +4,4 @@ import { BaseHeading } from "./BaseHeading";
 const Heading1 = props =>
     <BaseHeading as='h1' fontSize={[3, 4, 5]} {...props} />
 
-export { Heading as H1 }
+export { Heading1 as H1 }
